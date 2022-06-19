7e5082329f42fec2b82203bfe4712ef04f303105
igor <107351574+igorspasenkov@users.noreply.github.com>
2022-06-19 22:10:41 +0300

    first

---
+++ TextAnalyze/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TextAnalyze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAnalyze = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.column1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.column2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Location = new System.Drawing.Point(29, 29);
+            this.textBoxSource.Multiline = true;
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(585, 170);
+            this.textBoxSource.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Исходный текст";
+            // 
+            // buttonAnalyze
+            // 
+            this.buttonAnalyze.Location = new System.Drawing.Point(242, 205);
+            this.buttonAnalyze.Name = "buttonAnalyze";
+            this.buttonAnalyze.Size = new System.Drawing.Size(163, 45);
+            this.buttonAnalyze.TabIndex = 2;
+            this.buttonAnalyze.Text = "Анализ";
+            this.buttonAnalyze.UseVisualStyleBackColor = true;
+            this.buttonAnalyze.Click += new System.EventHandler(this.buttonAnalyze_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.column1,
+            this.column2,
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(29, 256);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(585, 226);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // column1
+            // 
+            this.column1.Text = "Символ";
+            this.column1.Width = 109;
+            // 
+            // column2
+            // 
+            this.column2.Text = "Частота  (в %)";
+            this.column2.Width = 160;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Количество повторений";
+            this.columnHeader1.Width = 175;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(620, 256);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(94, 45);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(620, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 494);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.buttonAnalyze);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSource);
+            this.Name = "Form1";
+            this.Text = "Анализ текста";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAnalyze;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader column1;
+        private System.Windows.Forms.ColumnHeader column2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TextAnalyze/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace TextAnalyze
+{
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int counter=0;
+        private void buttonAnalyze_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            for (int i =0;i<textBoxSource.Text.Length;i++)
+            {
+                char c = char.ToLower(textBoxSource.Text[i]);
+                if (char.IsLetter(c))
+                {
+                    if (!map.ContainsKey(c)) map.Add(c, 0);
+                    map[c]++;
+                    counter++;
+                }
+            }
+            map = map.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            for (int i=0;i<map.Count;i++)
+            {
+                var item = map.ElementAt(i);
+                float freq = item.Value / (float)counter *100;
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text  = item.Key.ToString();
+                lvi.SubItems.Add(freq.ToString());
+                lvi.SubItems.Add(item.Value.ToString());
+                listView1.Items.Add(lvi);
+            }
+            map.Clear();
+            counter = 0;
+        }
+
+        public Dictionary<char, int> map = new Dictionary<char, int>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxSource.Clear();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+    }
+}
